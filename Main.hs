fdc4da76eadb80f261792365636d037aa79895b9
Richard Pilz <richard.pilz.online@gmx.de>
2014-11-05 18:49:05 +0100

    Switching to HsOpenSSL for better performance.

--- Main.hs
+++ Main.hs
@@ -5,13 +5,13 @@ import HTTPFS
 import Common
 import CommandLine
 
+import Control.Monad
 import Network.HTTP.Types
 import Network.HTTP.Client
-import Network.HTTP.Client.TLS
-import Network.Connection
+import Network.HTTP.Client.OpenSSL
+import OpenSSL.Session
 import System.Fuse hiding (EntryType)
 import System.Environment
-import Data.Default
 import Options.Applicative
 
 checkServer :: FS -> IO ()
@@ -20,19 +20,26 @@ checkServer fs = do
   res <- httpNoBody req (manager fs)
   if responseStatus res /= status200
     then error $ "Error: " ++ show (responseStatus res)
-    else return ()      
+    else return ()
+
+mkContext :: Bool -> IO SSLContext
+mkContext novalidate = do
+  c <- context
+  contextSetDefaultCiphers c
+  contextSetCADirectory c "/etc/ssl/certs"
+  when (not novalidate) $
+    contextSetVerificationMode c $ VerifyPeer False False Nothing
+  return c
 
 main :: IO ()
-main = do
+main = withOpenSSL $ do
   let opts = info (helper <*> cmdArgs)
              ( fullDesc
                <> progDesc "A FUSE file system for webserver directory listings" )
   args <- execParser opts
 
-  let tlsSettings = if disableCertificateValidation args
-                    then def { settingDisableCertificateValidation = True }
-                    else def
-      settings = mkManagerSettings tlsSettings Nothing
+  let settings = opensslManagerSettings $ mkContext novalidate
+      novalidate = disableCertificateValidation args
       requestAdj = case basicAuth args of
         Nothing -> id
         Just (u, p) -> applyBasicAuth u p
