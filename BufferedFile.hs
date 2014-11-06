38271cce7316acad008222e4b2ee139defe33246
Richard Pilz <richard.pilz.online@gmx.de>
2014-11-07 00:34:29 +0100

    Simpler implementation of BufferedFile using io-streams.

--- BufferedFile.hs
+++ BufferedFile.hs
@@ -1,13 +1,15 @@
+{-# LANGUAGE BangPatterns #-}
 module BufferedFile where
 
 import Data.IORef
-import Data.Monoid
 import System.Posix (ByteCount, FileOffset)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
-import Data.ByteString.Lazy (toStrict)
-import Data.ByteString.Builder
 import Control.Concurrent.MVar
+import qualified System.IO.Streams as S
+import Data.Functor
+import Control.Monad
+import Control.Exception
 
 data BufferedFile = BufferedFile {
   readBufferedFile :: ByteCount -> FileOffset -> IO ByteString,
@@ -17,43 +19,50 @@ data BufferedFile = BufferedFile {
 makeBufferedFile :: (FileOffset -> IO (IO ByteString))
                  -> IO ()
                  -> IO BufferedFile
-makeBufferedFile gen close = do
-  let readSource f = go 0 where
-        remaining = toStrict . toLazyByteString
-        go n lo count =
-          if n >= count
-            then do
-              let bs = remaining lo
-                  (res, rest) = B.splitAt (fromIntegral count) bs
-              return (byteString rest, res)
-            else do
-              bs <- f
-              if B.null bs
-                then return (mempty, remaining lo)
-                else go (n + B.length bs) (lo <> byteString bs) count
-
-  source <- gen 0 >>= newIORef
-  counter <- newIORef 0
-  leftover <- newIORef mempty
+makeBufferedFile gen' close = do
+  let gen off = gen' off >>= makeStream
   
-  let readCount cur count = do
-        f <- readIORef source
-        lo <- readIORef leftover
-        (lo', res) <- readSource f lo (fromIntegral count)
-        writeIORef leftover lo'
-        writeIORef counter $ cur + fromIntegral (B.length res)
-        return res
-
+  source <- gen 0 >>= newIORef
+  pos <- newIORef 0
+  
   lock <- newMVar ()
   
   let readBuffered count off = withMVar lock $ \() -> do
-        cur <- readIORef counter
-        if off == cur
-           then readCount cur count
-           else do
-             gen off >>= writeIORef source
-             writeIORef counter off
-             writeIORef leftover mempty
-             readCount off count
+        cur <- readIORef pos
+        input <- if off == cur
+                 then readIORef source
+                 else do
+                   sourceNew <- gen off
+                   writeIORef source sourceNew
+                   return sourceNew
+        let count' = fromIntegral count
+        bs <- readAtMost count' input `onException` writeIORef pos (-1)
+        writeIORef pos $ off + fromIntegral (B.length bs)
+        return bs
 
   return $ BufferedFile readBuffered close
+
+
+makeStream :: IO ByteString -> IO (S.InputStream ByteString)
+makeStream f = S.makeInputStream (trans <$> f) where
+  trans bs | B.null bs = Nothing
+           | otherwise = Just bs
+
+-- copied and adapted from io-streams
+readAtMost :: Int                     -- ^ number of bytes to read
+            -> S.InputStream ByteString  -- ^ input stream
+            -> IO ByteString
+readAtMost n input = go id n
+  where
+    go !dl 0  = return $! B.concat $! dl []
+    go !dl k  =
+        S.read input >>=
+        maybe (return $! B.concat $! dl [])
+              (\s -> do
+                 let l = B.length s
+                 if l >= k
+                   then do
+                     let (a,b) = B.splitAt k s
+                     when (not $ B.null b) $ S.unRead b input
+                     return $! B.concat $! dl [a]
+                   else go (dl . (s:)) (k - l))
