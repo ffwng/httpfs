37d5095ebdf87a67fec660157c5410ff99c9bb82
Richard Pilz <richard.pilz.online@gmx.de>
2015-12-09 20:27:39 +0100

    Makes MemCache thread-safe using STM

--- MemCache.hs
+++ MemCache.hs
@@ -1,32 +1,36 @@
 module MemCache where
 
 import qualified Data.Map.Strict as M
-import Data.IORef
+import Control.Concurrent.STM
 import System.Posix.Types
 import System.Posix.Time
 
-data MemCache k a = MemCache EpochTime (IORef (M.Map k (EpochTime, a)))
+data MemCache k a = MemCache EpochTime (TVar (M.Map k (EpochTime, a)))
 
 newMemCache :: Int -> IO (MemCache k a)
-newMemCache d = MemCache (fromIntegral d) <$> newIORef M.empty
+newMemCache d = MemCache (fromIntegral d) <$> newTVarIO M.empty
 
 insertWith :: Ord k => (a -> a -> a) -> MemCache k a -> k -> a -> IO ()
 insertWith f (MemCache d ref) k a = do
   t <- epochTime
   let f' (tNew, aNew) (_, aOld) = (tNew, f aNew aOld)
-  modifyIORef ref $ M.insertWith f' k (t + d, a)
+  atomically . modifyTVar' ref $ M.insertWith f' k (t + d, a)
 
 insert :: Ord k => MemCache k a -> k -> a -> IO ()
 insert = insertWith const
 
 delete :: Ord k => MemCache k a -> k -> IO ()
-delete (MemCache _ ref) = modifyIORef ref . M.delete
+delete mc = atomically . deleteSTM mc
+
+deleteSTM :: Ord k => MemCache k a -> k -> STM ()
+deleteSTM (MemCache _ ref) = modifyTVar' ref . M.delete
 
 query :: Ord k => MemCache k a -> k -> IO (Maybe a)
 query mc@(MemCache _ ref) k = do
-  m <- readIORef ref
   t <- epochTime
-  case M.lookup k m of
-    Nothing -> return Nothing
-    Just (t', _) | t' < t -> delete mc k >> return Nothing
-    Just (_, b) -> return (Just b)
+  atomically $ do
+    m <- readTVar ref
+    case M.lookup k m of
+      Nothing -> return Nothing
+      Just (t', _) | t' < t -> deleteSTM mc k >> return Nothing
+      Just (_, b) -> return (Just b)
